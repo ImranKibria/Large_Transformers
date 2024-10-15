eb5864d0de00e9b022c5e8af1904372fb080972b
Imran Kibria <75063249+ImranKibria@users.noreply.github.com>
2024-10-15 14:00:09 -0400

    Add files via upload

---
+++ wav2vec/test.py
@@ -0,0 +1,170 @@
+import os
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.stats import pearsonr
+from scipy.stats import spearmanr
+from transformers import TrainingArguments, Trainer
+from datasets import load_dataset, load_metric, Audio
+from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2ForSequenceClassification
+
+###
+# hardware status
+###
+
+batch_size = 16
+
+print('Availability of torch: ', torch.cuda.is_available())
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print('Device for model training & evaluation: ', device)
+
+###
+# data preprocessing functions
+###
+
+parent_dir = '/fs/ess/PAS2301/Data/Speech/NISQA_Corpus'
+def update_filepath(example):
+  example['filepath_deg'] = os.path.join(parent_dir, example['filepath_deg'])
+  return example
+
+feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained('facebook/wav2vec2-base-960h')
+def preprocess_function(examples):
+  audio_array = [x["array"] for x in examples['filepath_deg']]
+  inputs = feature_extractor(
+    audio_array,
+    truncation=True,
+    sampling_rate=16000,
+    max_length=16000*10,
+    padding='max_length',
+    return_tensors = 'pt'
+  )
+  
+  labels = torch.tensor(examples['mos']).unsqueeze(-1).float()
+  return {
+    'input_values': inputs['input_values'],
+    'label': labels
+  }
+
+###
+# loading & preprocessing 5 testsets
+###
+
+print('\n*** NISQA VAL SIM ***')
+NISQA_VAL_SIM = load_dataset(path=parent_dir+"/NISQA_VAL_SIM", data_files='NISQA_VAL_SIM_file.csv')['train']
+NISQA_VAL_SIM = NISQA_VAL_SIM.map(update_filepath).cast_column("filepath_deg", Audio(sampling_rate=16000))
+NISQA_VAL_SIM = NISQA_VAL_SIM.map(preprocess_function, batched=True).select_columns(['input_values', 'label'])
+
+print('\n*** NISQA VAL LIVE ***')
+NISQA_VAL_LIVE = load_dataset(path=parent_dir+"/NISQA_VAL_LIVE", data_files='NISQA_VAL_LIVE_file.csv')['train']
+NISQA_VAL_LIVE = NISQA_VAL_LIVE.map(update_filepath).cast_column("filepath_deg", Audio(sampling_rate=16000))
+NISQA_VAL_LIVE = NISQA_VAL_LIVE.map(preprocess_function, batched=True).select_columns(['input_values', 'label'])
+
+print('\n*** NISQA TEST FOR ***')
+NISQA_TEST_FOR = load_dataset(path=parent_dir+"/NISQA_TEST_FOR", data_files='NISQA_TEST_FOR_file.csv')['train']
+NISQA_TEST_FOR = NISQA_TEST_FOR.map(update_filepath).cast_column("filepath_deg", Audio(sampling_rate=16000))
+NISQA_TEST_FOR = NISQA_TEST_FOR.map(preprocess_function, batched=True).select_columns(['input_values', 'label'])
+
+print('\n*** NISQA TEST P501 ***')
+NISQA_TEST_P501 = load_dataset(path=parent_dir+"/NISQA_TEST_P501", data_files='NISQA_TEST_P501_file.csv')['train']
+NISQA_TEST_P501 = NISQA_TEST_P501.map(update_filepath).cast_column("filepath_deg", Audio(sampling_rate=16000))
+NISQA_TEST_P501 = NISQA_TEST_P501.map(preprocess_function, batched=True).select_columns(['input_values', 'label'])
+
+print('\n*** NISQA TEST LIVETALK ***')
+NISQA_TEST_LIVETALK = load_dataset(path=parent_dir+"/NISQA_TEST_LIVETALK", data_files='NISQA_TEST_LIVETALK_file.csv')['train']
+NISQA_TEST_LIVETALK = NISQA_TEST_LIVETALK.map(update_filepath).cast_column("filepath_deg", Audio(sampling_rate=16000))
+NISQA_TEST_LIVETALK = NISQA_TEST_LIVETALK.map(preprocess_function, batched=True).select_columns(['input_values', 'label'])
+
+###
+# model loading from checkpoint 2249
+###
+
+model_checkpoint = "facebook/wav2vec2-base-960h"
+model = Wav2Vec2ForSequenceClassification.from_pretrained(model_checkpoint).to(device)
+
+hidden_size = model.classifier.in_features
+num_output_classes = 1
+model.classifier = torch.nn.Linear(hidden_size, num_output_classes)
+
+model.load_state_dict(torch.load("wav2vec2-base-960h-finetuned/checkpoint-519/pytorch_model.bin", map_location=torch.device(device)))
+print(model)
+
+###
+# arguments, compilation, & testing 
+###
+
+args = TrainingArguments(
+  './testing',                            # checkpoints saved here
+  evaluation_strategy = "epoch",          # evaluation at the end of epoch
+  save_strategy = "epoch",                # model saved at the end of epoch
+  learning_rate=5e-5,                     # value obtained from AST paper
+  per_device_train_batch_size=batch_size,
+  per_device_eval_batch_size=batch_size,
+  num_train_epochs=20,                    
+  logging_strategy = "epoch",             # results logged at the end of epoch
+  dataloader_num_workers = 8,             # parallelizing data fetch process
+  load_best_model_at_end=True,            # best model loaded after training
+  metric_for_best_model="mse",            
+  greater_is_better="False"               # specifying lower mse is better
+)
+
+###
+# model training and evaluation via rmse objective
+###
+
+metric = load_metric("mse")
+def compute_metrics(eval_pred):
+  """Computes accuracy on a batch of predictions"""
+  logits, labels = eval_pred
+  predictions = logits.squeeze() 
+  return metric.compute(predictions=predictions, references=labels)
+
+class CustomTrainer(Trainer):
+  def compute_loss(self, model, inputs, return_outputs=False):
+    labels = inputs.pop("labels")
+    outputs = model(**inputs)
+    logits = outputs.get("logits")
+    
+    loss_fn = torch.nn.MSELoss()
+    loss = loss_fn(logits.squeeze(), labels.squeeze())
+    return (loss, outputs) if return_outputs else loss
+
+trainer = CustomTrainer(
+  model=model,
+  args=args,
+  compute_metrics=compute_metrics,
+)
+
+testsets = ['NISQA_VAL_SIM', 'NISQA_VAL_LIVE', 'NISQA_TEST_FOR', 'NISQA_TEST_P501', 'NISQA_TEST_LIVETALK']
+for testset in testsets:
+    print('\nTestset under consideration: ', testset)
+
+    if testset == 'NISQA_VAL_SIM':
+      ds = NISQA_VAL_SIM
+    elif testset == 'NISQA_VAL_LIVE':
+        ds = NISQA_VAL_LIVE
+    elif testset == 'NISQA_TEST_FOR':
+        ds = NISQA_TEST_FOR
+    elif testset == 'NISQA_TEST_P501':
+        ds = NISQA_TEST_P501
+    elif testset == 'NISQA_TEST_LIVETALK':
+        ds = NISQA_TEST_LIVETALK
+    
+    output = trainer.predict(ds)
+    pred_mos = np.array(output[0]).flatten()
+    true_mos = np.array(output[1]).flatten()
+    
+    # plot & numerical analysis
+    rmse = (np.mean((true_mos-pred_mos)**2))**0.5
+    print('RMSE (utterance-level) = ', rmse)
+    p_corr, _ = pearsonr(true_mos, pred_mos)
+    print('Pearson Correlation Coefficient = ', p_corr)
+    s_corr, _ = spearmanr(true_mos, pred_mos)
+    print('Spearman Rank Correlation Coefficient = ', s_corr)
+
+    # SCATTER PLOT
+    plt.figure()
+    plt.scatter(pred_mos, true_mos)
+    plt.title(testset)
+    plt.xlim([1,5]); plt.xlabel('Predicted')
+    plt.ylim([1,5]); plt.ylabel('True MOS')
+    plt.savefig('testing/' + testset + '.png')
---
+++ wav2vec/train.py
@@ -0,0 +1,147 @@
+'Adapting wav2vec from Hugging Face for Speech Quality Assessment'
+
+import os
+import torch
+import librosa
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from transformers import TrainingArguments, Trainer
+from datasets import load_dataset, load_metric, DatasetDict, Audio
+from transformers import Wav2Vec2FeatureExtractor, Wav2Vec2ForSequenceClassification
+
+print('Availability of torch: ', torch.cuda.is_available())
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print('Device for model training & evaluation: ', device)
+
+batch_size = 16
+model_checkpoint = "facebook/wav2vec2-base-960h"
+
+###
+# load audio dataset from csv files
+###
+
+df_NISQA_TRAIN_SIM = pd.read_csv('/fs/ess/PAS2301/Data/Speech/NISQA_Corpus/NISQA_TRAIN_SIM/NISQA_TRAIN_SIM_file.csv')
+df_NISQA_TRAIN_LIVE = pd.read_csv('/fs/ess/PAS2301/Data/Speech/NISQA_Corpus/NISQA_TRAIN_LIVE/NISQA_TRAIN_LIVE_file.csv')
+
+train_df = pd.concat([df_NISQA_TRAIN_SIM, df_NISQA_TRAIN_LIVE], axis=0, ignore_index=True)
+train_df.to_csv('temp/NISQA_TRAIN.csv', index=False)
+
+df_NISQA_VAL_SIM = pd.read_csv('/fs/ess/PAS2301/Data/Speech/NISQA_Corpus/NISQA_VAL_SIM/NISQA_VAL_SIM_file.csv')
+df_NISQA_VAL_LIVE = pd.read_csv('/fs/ess/PAS2301/Data/Speech/NISQA_Corpus/NISQA_VAL_LIVE/NISQA_VAL_LIVE_file.csv')
+
+val_df = pd.concat([df_NISQA_VAL_SIM, df_NISQA_VAL_LIVE], axis=0, ignore_index=True)
+val_df.to_csv('temp/NISQA_VAL.csv', index=False)
+
+###
+# convert pandas dataframe to datasets format
+###
+
+train_ds = load_dataset(path="temp", data_files='NISQA_TRAIN.csv')['train']
+val_ds = load_dataset(path="temp", data_files='NISQA_VAL.csv')['train']
+
+combined_dataset = DatasetDict({
+  'train': train_ds,
+  'validation': val_ds,
+}).select_columns(['filepath_deg', 'mos'])
+print(f'\ncsv dataset:\n {combined_dataset}')
+
+###
+# sample degraded audio from its filepath
+###
+
+parent_dir = '/fs/ess/PAS2301/Data/Speech/NISQA_Corpus'
+def update_filepath(example):
+  example['filepath_deg'] = os.path.join(parent_dir, example['filepath_deg'])
+  return example
+
+combined_dataset = combined_dataset.map(update_filepath)
+audio_dataset = combined_dataset.cast_column("filepath_deg", Audio(sampling_rate=16000))
+print(f'\naudio dataset:\n {audio_dataset}')
+
+###
+# compute features (compatible wav2vec input) from raw audio.
+###
+
+feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(model_checkpoint)
+def preprocess_function(examples):
+  audio_array = [x["array"] for x in examples['filepath_deg']]
+  inputs = feature_extractor(
+    audio_array,
+    truncation=True,
+    sampling_rate=16000,
+    max_length=16000*10,
+    padding='max_length',
+    return_tensors = 'pt'
+  )
+  
+  labels = torch.tensor(examples['mos']).unsqueeze(-1).float()
+  return {
+    'input_values': inputs['input_values'],
+    'label': labels
+  }
+  
+encoded_dataset = audio_dataset.map(preprocess_function, batched=True).select_columns(['input_values', 'label']).shuffle()
+print(f'\nencoded dataset:\n {encoded_dataset}')
+
+###
+# load pretrained wav2vec with 1-unit head to predict mos
+###
+
+model = Wav2Vec2ForSequenceClassification.from_pretrained(model_checkpoint).to(device)
+
+hidden_size = model.classifier.in_features
+num_output_classes = 1
+model.classifier = torch.nn.Linear(hidden_size, num_output_classes)
+
+print(f'wav2vec2 model with MOS prediction head:\n {model}')
+
+###
+# model optimization and checkpointing
+###
+
+model_name = model_checkpoint.split("/")[-1]
+args = TrainingArguments(
+  f"{model_name}-finetuned",              # checkpoints saved here
+  evaluation_strategy = "epoch",          # evaluation at the end of epoch
+  save_strategy = "epoch",                # model saved at the end of epoch
+  learning_rate=5e-5,                     # value obtained from AST paper
+  per_device_train_batch_size=batch_size,
+  per_device_eval_batch_size=batch_size,
+  num_train_epochs=20,                    
+  logging_strategy = "epoch",             # results logged at the end of epoch
+  dataloader_num_workers = 8,             # parallelizing data fetch process
+  load_best_model_at_end=True,            # best model loaded after training
+  metric_for_best_model="mse",            
+  greater_is_better="False"               # specifying lower mse is better
+)
+
+###
+# model training and evaluation via rmse objective
+###
+
+metric = load_metric("mse")
+def compute_metrics(eval_pred):
+  """Computes accuracy on a batch of predictions"""
+  logits, labels = eval_pred
+  predictions = logits.squeeze() 
+  return metric.compute(predictions=predictions, references=labels)
+
+class CustomTrainer(Trainer):
+  def compute_loss(self, model, inputs, return_outputs=False):
+    labels = inputs.pop("labels")
+    outputs = model(**inputs)
+    logits = outputs.get("logits")
+    
+    loss_fn = torch.nn.MSELoss()
+    loss = loss_fn(logits.squeeze(), labels.squeeze())
+    return (loss, outputs) if return_outputs else loss
+
+trainer = CustomTrainer(
+  model=model,
+  args=args,
+  train_dataset=encoded_dataset['train'],
+  eval_dataset=encoded_dataset['validation'],
+  compute_metrics=compute_metrics,
+)
+trainer.train()
